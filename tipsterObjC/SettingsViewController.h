e5e457ef51166dad9f1ecc37a50778f932b9bb5b
Jigyasaa Sood <jsood@ucsd.edu>
2018-06-18 19:23:06 -0700

    finished required assignment

---
+++ tipsterObjC/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  tipsterObjC
+//
+//  Created by jsood on 6/18/18.
+//  Copyright © 2018 Jigyasaa Sood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
--- tipsterObjC/ViewController.m
+++ tipsterObjC/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercentSegments;
+
 @end
 
 @implementation ViewController
@@ -18,6 +23,32 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+- (IBAction)hideKeyboard:(UITapGestureRecognizer *)sender {
+    NSLog(@"hello");
+    [self.view endEditing:YES];
+}
+
+- (IBAction)editedBillAmount:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    
+    
+    NSArray * tipPercents = @[@(0.15), @(0.20), @(0.25)];
+    
+    double tipSegmentIndex = [tipPercents[self.tipPercentSegments.selectedSegmentIndex] doubleValue];
+    
+    double tip = bill * tipSegmentIndex;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+    
+}
+
+
+
+
 
 
 - (void)didReceiveMemoryWarning {
