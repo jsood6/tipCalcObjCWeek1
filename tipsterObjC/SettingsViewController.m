b7bd08250e80226146a0b004ab040fd2d300234d
Jigyasaa Sood <jsood@ucsd.edu>
2018-06-18 22:21:25 -0700

    added settings page

--- tipsterObjC/SettingsViewController.m
+++ tipsterObjC/SettingsViewController.m
@@ -9,20 +9,136 @@
 #import "SettingsViewController.h"
 
 @interface SettingsViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *firstTipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *secondTipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *thirdTipLabel;
+
 
 @end
 
 @implementation SettingsViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    double doubleValue1 = [defaults doubleForKey:@"default_tip1_percentage"];
+    NSString * tipVal1 = [NSString stringWithFormat:@"%.0f", doubleValue1];
+    self.firstTipLabel.text = tipVal1;
+    
+    double doubleValue2 = [defaults doubleForKey:@"default_tip2_percentage"];
+    NSString * tipVal2 = [NSString stringWithFormat:@"%.0f", doubleValue2];
+    self.secondTipLabel.text = tipVal2;
+    
+    double doubleValue3 = [defaults doubleForKey:@"default_tip3_percentage"];
+    NSString * tipVal3 = [NSString stringWithFormat:@"%.0f", doubleValue3];
+    self.thirdTipLabel.text = tipVal3;
     // Do any additional setup after loading the view.
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:YES];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    double doubleValue1 = [defaults doubleForKey:@"default_tip1_percentage"];
+    NSString * tipVal1 = [NSString stringWithFormat:@"%.0f", doubleValue1];
+    self.firstTipLabel.text = tipVal1;
+    
+    double doubleValue2 = [defaults doubleForKey:@"default_tip2_percentage"];
+    NSString * tipVal2 = [NSString stringWithFormat:@"%.0f", doubleValue2];
+    self.secondTipLabel.text = tipVal2;
+    
+    double doubleValue3 = [defaults doubleForKey:@"default_tip3_percentage"];
+    NSString * tipVal3 = [NSString stringWithFormat:@"%.0f", doubleValue3];
+    self.thirdTipLabel.text = tipVal3;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+- (IBAction)addBtnPressed:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    if([sender tag] == 0){
+        double firstTipVal =  [self.firstTipLabel.text doubleValue];
+        firstTipVal = firstTipVal + 1;
+        self.firstTipLabel.text = [NSString stringWithFormat:@"%.0f", firstTipVal];
+        [defaults setDouble:firstTipVal forKey:@"default_tip1_percentage"];
+        [defaults synchronize];
+    }
+    
+    else if([sender tag] == 1){
+        double secondTipVal =  [self.secondTipLabel.text doubleValue];
+        secondTipVal = secondTipVal + 1;
+        self.secondTipLabel.text = [NSString stringWithFormat:@"%.0f", secondTipVal];
+        [defaults setDouble:secondTipVal forKey:@"default_tip2_percentage"];
+        [defaults synchronize];
+    }
+    
+    else if([sender tag] == 2){
+        double thirdTipVal =  [self.thirdTipLabel.text doubleValue];
+        thirdTipVal = thirdTipVal + 1;
+        self.thirdTipLabel.text = [NSString stringWithFormat:@"%.0f", thirdTipVal];
+        [defaults setDouble:thirdTipVal forKey:@"default_tip3_percentage"];
+        [defaults synchronize];
+    }
+}
+
+- (IBAction)subtractBtnPressed:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    if([sender tag] == 0){
+        double firstTipVal =  [self.firstTipLabel.text doubleValue];
+        firstTipVal = firstTipVal - 1;
+        self.firstTipLabel.text = [NSString stringWithFormat:@"%.0f", firstTipVal];
+        [defaults setDouble:firstTipVal forKey:@"default_tip1_percentage"];
+        [defaults synchronize];
+    }
+    
+    else if([sender tag] == 1){
+        double secondTipVal =  [self.secondTipLabel.text doubleValue];
+        secondTipVal = secondTipVal - 1;
+        self.secondTipLabel.text = [NSString stringWithFormat:@"%.0f", secondTipVal];
+        [defaults setDouble:secondTipVal forKey:@"default_tip2_percentage"];
+        [defaults synchronize];
+    }
+    
+    else if([sender tag] == 2){
+        double thirdTipVal =  [self.thirdTipLabel.text doubleValue];
+        thirdTipVal = thirdTipVal - 1;
+        self.thirdTipLabel.text = [NSString stringWithFormat:@"%.0f", thirdTipVal];
+        [defaults setDouble:thirdTipVal forKey:@"default_tip3_percentage"];
+        [defaults synchronize];
+    }
+}
+
+
+
+
+- (IBAction)resetTipVals:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    double firstTipVal = 15;
+    self.firstTipLabel.text = [NSString stringWithFormat:@"%.0f", firstTipVal];
+    [defaults setDouble:firstTipVal forKey:@"default_tip1_percentage"];
+    [defaults synchronize];
+    
+    double secondTipVal = 20;
+    self.secondTipLabel.text = [NSString stringWithFormat:@"%.0f", secondTipVal];
+    [defaults setDouble:secondTipVal forKey:@"default_tip2_percentage"];
+    [defaults synchronize];
+    
+    double thirdTipVal = 25;
+    self.thirdTipLabel.text = [NSString stringWithFormat:@"%.0f", thirdTipVal];
+    [defaults setDouble:thirdTipVal forKey:@"default_tip3_percentage"];
+    [defaults synchronize];
+}
+
+
 
 /*
 #pragma mark - Navigation
--- tipsterObjC/ViewController.m
+++ tipsterObjC/ViewController.m
@@ -21,7 +21,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    double doubleValue1 = [defaults doubleForKey:@"default_tip1_percentage"];
+    NSString * tipVal1 = [NSString stringWithFormat:@"%.0f%%", doubleValue1];
+    
+    double doubleValue2 = [defaults doubleForKey:@"default_tip2_percentage"];
+    NSString * tipVal2 = [NSString stringWithFormat:@"%.0f%%", doubleValue2];
+    
+    double doubleValue3 = [defaults doubleForKey:@"default_tip3_percentage"];
+    NSString * tipVal3 = [NSString stringWithFormat:@"%.0f%%", doubleValue3];
+    
+    [self.tipPercentSegments setTitle:tipVal1 forSegmentAtIndex:0];
+    
+    [self.tipPercentSegments setTitle:tipVal2 forSegmentAtIndex:1];
+    
+    [self.tipPercentSegments setTitle:tipVal3 forSegmentAtIndex:2];
+    
+    NSLog(@"first tip is:%f", doubleValue1 );
+      NSLog(@"second tip is:%f", doubleValue2 );
+      NSLog(@"third tip is:%f", doubleValue3 );
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:YES];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    double doubleValue1 = [defaults doubleForKey:@"default_tip1_percentage"];
+    NSString * tipVal1 = [NSString stringWithFormat:@"%.0f%%", doubleValue1];
+    
+    double doubleValue2 = [defaults doubleForKey:@"default_tip2_percentage"];
+    NSString * tipVal2 = [NSString stringWithFormat:@"%.0f%%", doubleValue2];
+    
+    double doubleValue3 = [defaults doubleForKey:@"default_tip3_percentage"];
+    NSString * tipVal3 = [NSString stringWithFormat:@"%.0f%%", doubleValue3];
+    
+    [self.tipPercentSegments setTitle:tipVal1 forSegmentAtIndex:0];
+    
+    [self.tipPercentSegments setTitle:tipVal2 forSegmentAtIndex:1];
+    
+    [self.tipPercentSegments setTitle:tipVal3 forSegmentAtIndex:2];
+    
+    NSLog(@"first tip is:%.0f", doubleValue1 );
+    NSLog(@"second tip is:%.0f", doubleValue2 );
+    NSLog(@"third tip is:%.0f", doubleValue3 );
 }
 
 - (IBAction)hideKeyboard:(UITapGestureRecognizer *)sender {
@@ -30,10 +74,29 @@
 }
 
 - (IBAction)editedBillAmount:(id)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    double doubleValue1 = [defaults doubleForKey:@"default_tip1_percentage"];
+    NSString * tipVal1 = [NSString stringWithFormat:@"%.0f%%", doubleValue1];
+    
+    double doubleValue2 = [defaults doubleForKey:@"default_tip2_percentage"];
+    NSString * tipVal2 = [NSString stringWithFormat:@"%.0f%%", doubleValue2];
+    
+    double doubleValue3 = [defaults doubleForKey:@"default_tip3_percentage"];
+    NSString * tipVal3 = [NSString stringWithFormat:@"%.0f%%", doubleValue3];
+    
+    
+    [self.tipPercentSegments setTitle:tipVal1 forSegmentAtIndex:0];
+    
+    [self.tipPercentSegments setTitle:tipVal2 forSegmentAtIndex:1];
+    
+    [self.tipPercentSegments setTitle:tipVal3 forSegmentAtIndex:2];
+    
+    
     double bill = [self.billField.text doubleValue];
     
     
-    NSArray * tipPercents = @[@(0.15), @(0.20), @(0.25)];
+    //NSArray * tipPercents = @[@(floorf(doubleValue1 * 100) / 100), @(floorf(doubleValue2 * 100) / 100), @(floorf(doubleValue3 * 100) / 100)];
+    NSArray * tipPercents = @[@(doubleValue1/ 100), @(doubleValue2/ 100), @(doubleValue3/ 100)];
     
     double tipSegmentIndex = [tipPercents[self.tipPercentSegments.selectedSegmentIndex] doubleValue];
     
